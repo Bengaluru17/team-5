766eea3b767283632b59fe4bf105202b3965d230
Karan Uppal <karan.uppal@justride.in>
2017-07-08 17:42:14 +0530

    karan-master-copy #comment validation done for admin login

--- web/admin/login.php
+++ web/admin/login.php
@@ -9,9 +9,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
     <title>Login</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="loginstyle.scss" rel="stylesheet">
 </head>
 
--- web/admin/validate.php
+++ web/admin/validate.php
@@ -6,19 +6,16 @@ $result = $conn->query($check);
 
 if (mysqli_num_rows($result) == 1)
 {
-    echo "Successful login. Redirecting to the home page in 3 seconds.\n";
+    // echo "Successful login. Redirecting to the home page in 3 seconds.\n";
     $row = mysqli_fetch_row($result);
-    $aid_new = $row[0];
+    //    $aid_new = $row[0];
     $username_new = $row[1];
-
-    $_SESSION['aid']= $aid_new;
     $_SESSION['username']= $username_new;
     echo "<script>alert('Success!');</script>";
     header("Location: index.php");
 }
-
 else
-{   echo "<script>alert('No success!');</script>";
+{   // echo "<script>alert('No success! Please use the correct details.');</script>";
     header('Location: login.php');
 }
 ?>
