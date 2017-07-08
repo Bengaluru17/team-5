890fe602bf5089e4a6ce61301ba5ebbf0404fb63
Karan Uppal <karan.uppal@justride.in>
2017-07-08 16:59:57 +0530

    karan-master-copy #comment modified sidebar

--- web/admin/sidebar.php
+++ web/admin/sidebar.php
@@ -3,7 +3,14 @@
         Reaching Hands
     </div>
     <div class="profile-element">
-
     </div>
-
+    <div class="other-element">
+        Option 1
+    </div>
+    <div class="other-element">
+        Option 2
+    </div>
+    <div class="other-element">
+        Option 3
+    </div>
 </div>
