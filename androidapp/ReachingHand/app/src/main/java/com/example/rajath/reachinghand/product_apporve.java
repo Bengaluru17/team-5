756508fa3a2c4e2203b52036b5c6781cabb4047c
kira1508 <kiranhebbar96@gmail.com>
2017-07-08 16:42:26 +0530

    FireBase

--- androidapp/ReachingHand/app/src/main/java/com/example/rajath/reachinghand/product_apporve.java
+++ androidapp/ReachingHand/app/src/main/java/com/example/rajath/reachinghand/product_apporve.java
@@ -2,6 +2,8 @@ package com.example.rajath.reachinghand;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class product_apporve extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class product_apporve extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_apporve);
+        Button apporve;
+        apporve=(Button) findViewById(R.id.submit);
+        apporve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
