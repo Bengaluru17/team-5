49b9de4ac0305b4aa1eb0111d9d513eb0bec8fb5
Karan Uppal <karan.uppal@justride.in>
2017-07-08 19:17:08 +0530

    karan-master-copy #comment add user page

--- web/admin/add-user.php
+++ web/admin/add-user.php
@@ -1,30 +1,39 @@
 <?php
 include 'index.php';
+if(isset($_POST['username']))
+{
+    $user_username = $_POST['username'];
+    $user_password = $_POST['password'];
+    $user_fname = $_POST['fname'];
+    $user_lname = $_POST['lname'];
+//    $user_email = $_POST['email'];
+//    $user_contact = $_POST['contact'];
+    $insert = "INSERT INTO login (`username`, `password`, `first_name`, `last_name`)
+                VALUES ('$user_username','$user_password','$user_fname','$user_lname')";
+    if ($conn->query($insert) == TRUE) {
+        $msg = "Customer added successfully.";
+    } else {
+        $msg = "Customer addition unsuccessful.";
+    }
+}
+
 ?>
     <div class="main-body">
         <div class="pagetitle">
             Add User
         </div>
-        <div class="login">
-            <div class="login-triangle"></div>
-            <h2 class="login-header">Add Customer</h2>
-            <form name="RegForm" class="login-container" method="POST" action="add-customer.php" onsubmit="return validateForm()">
-
-                You are allowed to add customers for Zone <?php echo $cur_zone;?> only.
-                <strong><?php echo $msg;?></strong>
-                <strong><?php echo $emailErr;?></strong>
+        <div class="addform">
+            <form name="RegForm" class="login-container" method="POST" action="add-user.php" onsubmit="return validateAddForm()">
+<!--                <strong>--><?php //echo $msg;?><!--</strong>-->
                 <p><input id="fname" name="fname" type="text" placeholder="First Name*" required></p>
                 <p><input id="lname" name="lname" type="text" placeholder="Last Name*" required></p>
-                <p><input id="email" name="email" type="email" placeholder="Email*" required></p>
-                <p><input id="contact" name="contact" type="text" placeholder="Contact (10 digits)*" required></p>
-                <p><input id="zone" name="zone" type="text" placeholder="Zone*" value="Zone <?php echo $cur_zone;?>" disabled></p>
+<!--                <p><input id="email" name="email" type="email" placeholder="Email*" required></p>-->
+<!--                <p><input id="contact" name="contact" type="text" placeholder="Contact (10 digits)*" required></p>-->
                 <p><input id="username" name="username" type="text" placeholder="Username (4-10 characters)*" required></p>
                 <p><input id="password" name="password" type="password" placeholder="Password (min 6 characters)*" required></p>
                 <p><input type="hidden" value="Yes" name="AddCust" >
-                    <button type="submit" class="btn btn-primary">Add Customer</button></p>
-                <a href="index.php">Go Back</a>
+                    <button type="submit" class="btn btn-primary">Add User</button></p>
             </form>
-
         </div>
     </div>
 </body>
